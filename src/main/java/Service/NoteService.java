20c6c7a0d215411aacb371900d831ba2a2ce0803
NinaSemnul <bacardi.jewel@gmail.com>
2023-09-28 13:49:10 +0300

    Initial commit

---
+++ src/main/java/Service/NoteService.java
@@ -0,0 +1,55 @@
+package Service;
+
+import Entity.Note;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class NoteService {
+
+    private final Map<Long, Note> noteMap = new HashMap<>();
+    private long nextId = 1;
+
+    public List<Note> listAll() {
+        return new ArrayList<>(noteMap.values());
+    }
+
+    public Note add(Note note) {
+        long id = generateId();
+        note.setId(id);
+        noteMap.put(id, note);
+        return note;
+    }
+
+    public void deleteById(long id) {
+        if (!noteMap.containsKey(id)) {
+            throw new IllegalArgumentException("Note with id " + id + " not found");
+        }
+        noteMap.remove(id);
+    }
+
+    public void update(Note note) {
+        long id = note.getId();
+        if (!noteMap.containsKey(id)) {
+            throw new IllegalArgumentException("Note with id " + id + " not found");
+        }
+        noteMap.put(id, note);
+    }
+
+    public Note getById(long id) {
+        if (!noteMap.containsKey(id)) {
+            throw new IllegalArgumentException("Note with id " + id + " not found");
+        }
+        return noteMap.get(id);
+    }
+
+    private synchronized long generateId() {
+        return nextId++;
+    }
+
+}
+
+
